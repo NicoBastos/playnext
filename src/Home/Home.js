11f84a1e53e1196d60674e6c1e749b8355f5f834
caleb-j-kim <coolycaleb@gmail.com>
2023-11-04 21:22:05 -0500

    added questionnaire

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,17 @@
 import './Home.css'
 import Login from './Pages/Login/Login'
 import Queue from './Pages/Queue/Queue'
+import Questionnaire from './Pages/Questionnaire/Questionnaire'
 import { Route, Routes, Link } from 'react-router-dom'
 
 function Home () {
   return (
     <div className='Home'>
       <p>Home page</p>
-      <Link to='/queue'>
+      <Link to = '/questionnaire'>
+        <button>questionnaire</button>
+        </Link>
+      <Link to ='/queue'>
         <button>Go to Queue</button>
       </Link>
       <Link to='/'>
@@ -16,6 +20,7 @@ function Home () {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/queue' element={<Queue />} />
+        <Route path = '/questionnaire' element={<Questionnaire />} />
       </Routes>
     </div>
   )
