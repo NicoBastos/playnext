6577d3b39aad5bd424f440b2a7cbd959664f3b36
caleb-j-kim <coolycaleb@gmail.com>
2023-11-05 01:43:14 -0600

    updated css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css'
 import Home from './Home/Home'
 import { BrowserRouter } from 'react-router-dom'
+import logo from './Images/Spotify_Logo_RGB_Green.png';
 
 function App () {
   return (
     <BrowserRouter>
+
       <Home />
+
+      <img src = {logo} className = "SpotifyLogo" alt = ""
+    style={{ width: '262.4px', height: '78.px' }}/>
     </BrowserRouter>
+
   )
 }
+
 
 export default App
