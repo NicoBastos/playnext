a0aed534a7c77f35ad6d2b22e365b9ff736c29e5
nicolas bastos <nickbastos4@gmail.com>
2023-11-04 21:58:07 -0500

    TOKEN PASSED DOWN JEJEJE

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,45 +1,56 @@
 import './Home.css'
-import {useState, useEffect} from 'react'
+import React, { useState, useEffect, createContext } from 'react'
 import Queue from './Pages/Queue/Queue'
-import { Route, Routes, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+
+export const TokenContext = createContext(null) // Export TokenContext
 
 function Home () {
-  const CLIENT_ID = "cc29af2c2de44d4abe0a84925167e99f"
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https:/accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
+  const CLIENT_ID = 'cc29af2c2de44d4abe0a84925167e99f'
+  const REDIRECT_URI = 'http://localhost:3000'
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize' // Fix typo in URL
+  const RESPONSE_TYPE = 'token'
 
-  const [token, setToken] = useState("")
+  const [token, setToken] = useState('')
 
   useEffect(() => {
     const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
+    let token = window.localStorage.getItem('token')
 
     if (!token && hash) {
-      token = hash.substring(1).split("&").find(element => element.startsWith("access_token")).split("=")[1]
+      token = hash
+        .substring(1)
+        .split('&')
+        .find(element => element.startsWith('access_token'))
+        .split('=')[1]
 
-      window.location.hash = ""
-      window.localStorage.setItem("token", token)
+      window.location.hash = ''
+      window.localStorage.setItem('token', token)
     }
 
     setToken(token)
   }, [])
 
   return (
-  <>
-    <div className='Home'>
-      <p>Home page</p>
-      <Link to='/queue'>
-        <button>Go to Queue</button>
-      </Link>
+    <>
+      <div className='Home'>
+        <p>Home page</p>
+        <Link to='/queue'>
+          <button>Go to Queue</button>
+        </Link>
         <button>Go to login</button>
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-      <Routes>
-        <Route 
-          path='/queue' element={<Queue />} />
-      </Routes>
-    </div>
-  </>
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          Login to Spotify
+        </a>
+        <TokenContext.Provider value={token}>
+          <Routes>
+            <Route path='/queue' element={<Queue />} />
+          </Routes>
+        </TokenContext.Provider>
+      </div>
+    </>
   )
 }
 
--- src/Home/Pages/Queue/Queue.js
+++ src/Home/Pages/Queue/Queue.js
@@ -1,42 +1,33 @@
 import './Queue.css'
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { BrowserRouter } from 'react-router-dom'
-function Queue (props, token) {
-  // five songs in the queue after api call
-  const [tracks, setTracks] = useState([]);
-  const [loading, setLoading] = useState(true);
+import React, { useState, useEffect, useContext } from 'react'
+import axios from 'axios'
+import { TokenContext } from '../../Home' // Import TokenContext
 
-  //const genre = props.genre;
+function Queue (props) {
+  // Add props as parameter
+  const [tracks, setTracks] = useState([])
+  const [loading, setLoading] = useState(true)
+  const token = useContext(TokenContext) // Use TokenContext
+
   useEffect(() => {
-    // Join the genres array into a comma-separated string for the seed_genres parameter
-    const genres = [props.genres];
-    let seedGenres;
-    if (genres.length > 1) {
-      seedGenres = genres.join(',');
-    }
-    else {
-      seedGenres = genres;
-    }
+    const genres = [props.genres]
+    let seedGenres = genres.length > 1 ? genres.join(',') : genres
 
-    axios.get('https://api.spotify.com/v1/recommendations', {
-      params: {
-        seed_genres: seedGenres, // Pass the seed_genres as a query parameter
-        // You can also include other query parameters here if needed
-      },
-      headers: {
-        Authorization: 'Bearer YOUR_ACCESS_TOKEN', // Replace with your Spotify access token
-      },
-    })
+    axios
+      .get('https://api.spotify.com/v1/recommendations', {
+        params: { seed_genres: seedGenres },
+        headers: { Authorization: `Bearer ${token}` } // Use token from context
+      })
       .then(response => {
-        setTracks(response.tracks); // Assuming the response contains an array of tracks
-        setLoading(false);
+        setTracks(response.data.tracks) // Access tracks from response.data.tracks
+        setLoading(false)
       })
       .catch(error => {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      });
-  }, [props]); // Include genres in the dependency array to trigger the request when it changes
+        console.error('Error fetching data:', error)
+        setLoading(false)
+      })
+  }, [props])
+  console.log(token)
   return (
     <div className='Queue'>
       This is the queue page
@@ -46,8 +37,7 @@ function Queue (props, token) {
         ))}
       </ul>
     </div>
-    
-  );
+  )
 }
 
 export default Queue
